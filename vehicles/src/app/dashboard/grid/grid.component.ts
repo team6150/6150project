5ed8faa6e3cc4a41b85983ffd1365107cd2d8068
Autumnqc <33812386+Autumnqc@users.noreply.github.com>
2017-12-02 14:39:28 -0500

    123

--- vehicles/src/app/dashboard/grid/grid.component.ts
+++ vehicles/src/app/dashboard/grid/grid.component.ts
@@ -4,4 +4,35 @@ import {Component} from '@angular/core';
     templateUrl: './grid.component.html'
 })
 
-export class GridComponent {}
+export class GridComponent {
+  myInterval = 5000;
+  index = 0;
+  slides: Array<any> = [];
+  imgUrl: Array<any> = [
+    `assets/img/slider1.jpg`,
+    `assets/img/slider2.jpg`,
+    `assets/img/slider3.jpg`,
+    `assets/img/slider0.jpg`
+  ];
+  /* END */
+  /* Alert component */
+  
+  /* END*/
+
+  constructor() {
+    for (let i = 0; i < 4; i++) {
+      this.addSlide();
+    }
+  }
+
+  /* Carousel */
+  addSlide() {
+    let i = this.slides.length;
+    this.slides.push({
+      image: this.imgUrl[i],
+      text: `${['Dummy ', 'Dummy ', 'Dummy ', 'Dummy '][this.slides.length % 4]}
+            ${['text 0', 'text 1', 'text 2', 'text 3'][this.slides.length % 4]}`
+    });
+  }
+  /* END */
+}
